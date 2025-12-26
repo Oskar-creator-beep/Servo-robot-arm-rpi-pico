0a280f38b01a305217f8ad785b6038580cdcd8ed
Oskar-creator-beep <mythic1pro@gmail.com>
2025-12-26 11:06:17 +0100

    before uf2 update

--- code.py
+++ code.py
@@ -1,23 +1,52 @@
 import board
-import time
 import digitalio
 import pwmio
 from adafruit_motor import servo
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+from adafruit_bluefruit_connect.packet import Packet
+from adafruit_bluefruit_connect.button_packet import ButtonPacket
+
+ble = BLERadio()
+uart_service = UARTService()
+advertisement = ProvideServicesAdvertisement(uart_service)
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-pwm = pwmio.PWMOut(board.GP16, duty_cycle=2 ** 15, frequency=50)
-moje_servo = servo.Servo(pwm)
+pwm1 = pwmio.PWMOut(board.GP16, duty_cycle=2 ** 15, frequency=50)
+servo1 = servo.Servo(pwm1, min_pulse=500, max_pulse=2500)
+
+pwm2 = pwmio.PWMOut(board.GP17, duty_cycle=2 ** 15, frequency=50)
+servo2 = servo.Servo(pwm2, min_pulse=500, max_pulse=2500)
 
 while True:
-    led.value = True
-    moje_servo.angle = 0
-    print("Kąt: 0")
-    time.sleep(1.0)
+    ble.start_advertising(advertisement)
 
-    led.value = False
-    moje_servo.angle = 180
-    print("Kąt: 180")
-    time.sleep(1.0)
+    while not ble.connected:
+        pass
+
+    while ble.connected:
+        if uart_service.in_waiting:
+            packet = Packet.from_stream(uart_service)
+            if isinstance(packet, ButtonPacket):
+                if packet.pressed:
+                    if packet.button == ButtonPacket.UP:
+                        led.value = True
+                        servo1.angle = 90
+                        servo2.angle = 90
+                    elif packet.button == ButtonPacket.DOWN:
+                        led.value = False
+                        servo1.angle = 0
+                        servo2.angle = 0
+    # led.value = True
+    # servo1.angle = 0
+    # servo2.angle = 0
+    # time.sleep(1.0)
+
+    # led.value = False
+    # servo1.angle = 180
+    # servo2.angle = 180
+    # time.sleep(1.0)
 
